807bf4d2e4b3e3eb25f251f9efea599f4f810fc7
amandreev17 <a_m_andreev2005@mail.ru>
2024-03-31 17:33:15 +0300

    remove using namespase std

--- timp-lab02/hello-world.cpp
+++ timp-lab02/hello-world.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
 int main()
 {
-    string name;
-    cin >> name;
-    cout << "Hello, world " << name << endl;
+    std::string name;
+    std::cin >> name;
+    std::cout << "Hello, world " << name << std::endl;
 }
