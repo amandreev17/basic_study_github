9313ef9c7f5fea4799220cdc4501731f26643634
amandreev17 <a_m_andreev2005@mail.ru>
2024-03-31 21:40:25 +0300

    Mozilla codestyle

--- timp-lab02/hello-world.cpp
+++ timp-lab02/hello-world.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    std::string name;
-    std::cin >> name;                                  // введите имя
-    std::cout << "Hello, world " << name << std::endl; // вывод имя
+  std::string name;
+  std::cin >> name;                                  // введите имя
+  std::cout << "Hello, world " << name << std::endl; // вывод имя
 }
