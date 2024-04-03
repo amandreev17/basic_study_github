d9dbe0fcb80f615a9d22cca739945faa4325672f
amandreev17 <144845041+amandreev17@users.noreply.github.com>
2024-04-03 10:27:29 +0300

    Mozilla codestyle

--- timp-lab02/hello-world.cpp
+++ timp-lab02/hello-world.cpp
@@ -6,4 +6,5 @@ int main()
   std::string name;
   std::cin >> name;                                  // введите имя
   std::cout << "Hello, world " << name << std::endl; // вывод имя
+  return 0;
 }
